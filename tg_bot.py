d0e11175ee928c542265cb987d0c5424da7ec5f5
dim0ntei <100030242@edu.miigaik.ru>
2022-08-10 02:00:10 +0300

    Adapting for tg

--- tg_bot.py
+++ tg_bot.py
@@ -1,5 +1,8 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher import FSMContext
 import tg_keyboards
 import config
 import logging
@@ -15,6 +18,37 @@ async def send_welcome(message: types.Message):
     await message.answer('Привет! Это тестовый бот сообщества ВК "МИИГАИК - лучший вуз"')
     await message.answer('ММИГАиК - Лучший вуз?',
                          reply_markup=tg_keyboards.hello)
+
+
+@dp.message_handler(Text(equals='Да!'))
+@dp.message_handler(Text(equals='Определенно да'))
+@dp.message_handler(Text(equals='Вне всяких сомнений'))
+async def start_menu(message: types.Message):
+    await message.answer('Основное меню',
+                         reply_markup=tg_keyboards.main_menu)
+
+
+class SearchByOption(StatesGroup):
+    faculty = State
+    year = State
+    sem = State
+    group = State
+    var = State
+    confirmation = State
+    end = State
+    error = State
+    sen_error = State
+
+
+available_faculty = ['ФГиИБ', 'ГФ', 'КФ', 'ФОП', 'АРХ', 'ФУТ']
+
+
+@dp.message_handler(Text(equals='Поиск по варианту', ignore_case=True))
+async def search_by_option(message: types.Message):
+    await message.answer('[Об этой функции](https://telegra.ph/Funkciya-poisk-po-variantu-08-09)\n'
+                         'Выбери свой факультет',
+                         parse_mode="Markdown",
+                         reply_markup=tg_keyboards.faculty)
 
 
 @dp.message_handler()
--- tg_keyboards.py
+++ tg_keyboards.py
@@ -2,9 +2,35 @@ from aiogram.types import ReplyKeyboardRemove, \
     ReplyKeyboardMarkup, KeyboardButton, \
     InlineKeyboardMarkup, InlineKeyboardButton
 
-# Кнопки
-YES = KeyboardButton('Да!',)
-actually = KeyboardButton('Определенно да')
-no_doubt = KeyboardButton('Вне всяких сомнений')
-# Клавиатуры
-hello = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(YES, actually, no_doubt)
+# Обычные кнопки
+YES = KeyboardButton(text='Да!', )
+actually = KeyboardButton(text='Определенно да')
+no_doubt = KeyboardButton(text='Вне всяких сомнений')
+search_by_option = KeyboardButton(text='Поиск по варианту')
+custom_works = KeyboardButton(text='Работы на заказ')
+extra = KeyboardButton(text='Дополнительно')
+
+# Инлайн кнопки
+back = InlineKeyboardButton(text='Назад', callback_data='back')
+cancel = InlineKeyboardButton(text='Отмена', callback_data='cancel')
+FGiIB = InlineKeyboardButton(text='ФГиИБ', callback_data='FGiIB')
+GF = InlineKeyboardButton(text='ГФ', callback_data='GF')
+KF = InlineKeyboardButton(text='КФ', callback_data='KF')
+FOP = InlineKeyboardButton(text='ФОП', callback_data='FOP')
+ARH = InlineKeyboardButton(text='АРХ', callback_data='ARH')
+FUT = InlineKeyboardButton(text='ФУТ', callback_data='FUT')
+year_20_21 = InlineKeyboardButton(text='2020-2021', callback_data='year_20_21')
+year_21_22 = InlineKeyboardButton(text='2021-2022', callback_data='year_21_22')
+
+
+# Обычные клавиатуры
+hello = InlineKeyboardMarkup(resize_keyboard=True,
+                             one_time_keyboard=True,
+                             row_width=2).add(YES, actually).row(no_doubt)
+main_menu = InlineKeyboardMarkup(resize_keyboard=True,
+                                 one_time_keyboard=True,
+                                 row_width=2).add(search_by_option).row(custom_works).row(extra)
+
+# Инлайн клавиатуры
+faculty = InlineKeyboardMarkup().row(FGiIB, GF).row(KF, FOP).row(ARH, FUT).add(back)
+year = InlineKeyboardMarkup().row(year_20_21, year_21_22)
